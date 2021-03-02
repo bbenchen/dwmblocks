5faf5e59deeb744a5f27da512b3707fa24a0ed44
Mike Chen <517926804@qq.com>
2021-03-02 18:18:13 +0800

    Update

--- blocks.def.h
+++ blocks.def.h
@@ -1,11 +1,16 @@
+/* macro for conveniently specifying pathu and pathc below */
+#define PATH(name) "/home/chenxianbin/Projects/desktop/suckless/dwmblocks/blocks/"name
+
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-
-	{"", "date '+%b %d (%a) %I:%M:%S%p'",					1,		0},
+	{"", PATH("nettraf.sh"),	1,                  1},
+	{"", PATH("cpu.sh"),	    5,                  4},
+	{"", PATH("memory.sh"),	    30,                 2},
+	{"", PATH("volume.sh"),     0,                  10},
+	{"", PATH("clock.sh"),      60,                 3},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = " | ";
+static char delim[] = " ";
 static unsigned int delimLen = 10;
