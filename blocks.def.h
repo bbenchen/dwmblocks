52b6f58379f5b8771c64ac67fd269da00f51ed72
Mike Chen <517926804@qq.com>
2021-03-03 13:27:31 +0800

    Move script file to ~/.local/bin

--- blocks.def.h
+++ blocks.def.h
@@ -1,14 +1,11 @@
-/* macro for conveniently specifying pathu and pathc below */
-#define PATH(name) "/home/chenxianbin/Projects/desktop/suckless/dwmblocks/blocks/"name
-
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", PATH("nettraf.sh"),	1,                  1},
-	{"", PATH("cpu.sh"),	    5,                  4},
-	{"", PATH("memory.sh"),	    30,                 2},
-	{"", PATH("volume.sh"),     0,                  10},
-	{"", PATH("clock.sh"),      60,                 3},
+	{"", "nettraf.sh",	1,                  1},
+	{"", "cpu.sh",	    5,                  4},
+	{"", "memory.sh",	    30,                 2},
+	{"", "volume.sh",     0,                  10},
+	{"", "clock.sh",      60,                 3},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
